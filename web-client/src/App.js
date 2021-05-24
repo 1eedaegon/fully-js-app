38a06d9aee5295462add475b4fc660ccc2ea700d
1eedaegon <d8726243@gmail.com>
2021-05-24 13:51:13 +0900

    feat: Set apollo client using dotenv
    CRA는 사용자의 실수를 방지하기 위해 process.env.NODE_ENV와
    REACT_APP_ 접두사로 시작하는 .env 환경변수를 지원한다.
    이외의 환경변수는 모두 무시하는데, 그 이유는 잦은 개인키 노출 방지와
    보안성을 강제하려고 하는 것이다.
    
    ref: https://github.com/facebook/create-react-app/issues/865#issuecomment-252199527

--- web-client/src/App.js
+++ web-client/src/App.js
@@ -2,7 +2,7 @@ import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import GlobalStyle from "./components/GlobalStyle";
 import Pages from "./pages";
 
-const uri = process.env.API_URI;
+const uri = process.env.REACT_APP_API_URI;
 const cache = new InMemoryCache();
 const client = new ApolloClient({
   uri,
@@ -13,10 +13,8 @@ const client = new ApolloClient({
 const App = () => {
   return (
     <ApolloProvider client={client}>
-      <div className="App">
-        <GlobalStyle />
-        <Pages />
-      </div>
+      <GlobalStyle />
+      <Pages />
     </ApolloProvider>
   );
 };
--- web-client/src/pages/home.js
+++ web-client/src/pages/home.js
@@ -1,12 +1,39 @@
+import { useQuery } from "@apollo/client";
+import gql from "graphql-tag";
 import Button from "../components/button";
 
-const home = () => {
+const GET_NOTES = gql`
+  query NoteFeed($cursor: String) {
+    noteFeed(cursor: $cursor) {
+      cursor
+      hasNextPage
+      notes {
+        id
+        content
+        createdAt
+        favoriteCount
+        author {
+          id
+          username
+          email
+        }
+      }
+    }
+  }
+`;
+
+const Home = () => {
+  const { data, loading, error, fetchMore } = useQuery(GET_NOTES);
+  if (loading) return <p>Now loading....</p>;
+  if (error) return <p>Error!</p>;
   return (
     <div className="home">
       <h2>This is Home page</h2>
       <Button>Click Please!</Button>
+      {console.log(data)}
+      <p>Data loaded!</p>
     </div>
   );
 };
 
-export default home;
+export default Home;
