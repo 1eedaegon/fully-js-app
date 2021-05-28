b7e5b32d76ca58b02795bdf5d8dee230dc6e3625
1eedaegon <d8726243@gmail.com>
2021-05-28 17:23:29 +0900

    feat: Update sign up
    uri로 http헤더를 생성하고 jwt토큰을 헤더에 추가해서
    전송하는 로직 추가

--- web-client/src/App.js
+++ web-client/src/App.js
@@ -1,11 +1,28 @@
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import {
+  ApolloClient,
+  ApolloProvider,
+  createHttpLink,
+  InMemoryCache,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 import GlobalStyle from "./components/GlobalStyle";
 import Pages from "./pages";
 
 const uri = process.env.REACT_APP_API_URI;
+const httpLink = createHttpLink({ uri });
 const cache = new InMemoryCache();
+
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      authorization: localStorage.getItem("token") || "",
+    },
+  };
+});
+
 const client = new ApolloClient({
-  uri,
+  link: httpLink.concat(authLink),
   cache,
   connectToDevTools: true,
 });
